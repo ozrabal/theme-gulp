08e6cd1ed55717b6a5847dfb318aff7390bf2871
Piotr <ozrabal@gmail.com>
2016-06-30 16:03:28 +0200

    cleaning

--- gulp/_default.js
+++ gulp/_default.js
@@ -1,10 +1,9 @@
-global.themeName = 'amuz' ;
-global.siteURL = 'http://teampower.dv';
+global.themeName = 'theme' ;
+global.siteURL = 'http://theme.dv';
 global.paths = {
     src: '_src',
     dist: '_static',
     theme: '_theme',
-
 
     scriptSrc: '_src/js/src/**/*.js',
     scriptsAllFile: 'scripts.all.js',
@@ -18,8 +17,6 @@ global.paths = {
     styleCssVendorAllFile: 'vendors.all.css',
     styleDist: '_static/css',
     styleDev: '_theme',
-
-
 
     vectorSrc: ['_src/img/**/*.svg', '!_src/img/src/**'],
     vectorTheme: '_theme/img',
@@ -59,36 +56,29 @@ var
     browserSync = require('browser-sync'),
     runSequence = require('run-sequence').use(gulp);
 
-
+//static
 gulp.task('default', function(){
     runSequence(['scripts-concat', 'vendor-concat', 'styles-vendor-concat', 'styles-sass'],'html-dev', 'browser-sync',  function () {
        gulp.watch(global.paths.scriptSrc, ['scripts-concat']);
         gulp.watch(global.paths.vendorSrc + '/**/*.js', ['vendor-concat']);
         gulp.watch(global.paths.styleSrc, ['styles-sass']);
-        //gulp.watch(global.paths.styleSrc + '/**/*.css', ['styles', 'styles-dev']);
-        //gulp.watch(global.paths.spriteSrc, ['sprite']);
-        //gulp.watch(global.paths.vectorSrc, ['vectors']);
-        //gulp.watch(global.paths.imagesSrc, ['images']);
         gulp.watch(global.paths.htmlSrc + '/**/*.html', ['html-dev', browserSync.reload]);
 
     });
 });
 
-
+//WordPress theme
 gulp.task('theme', function(){
     runSequence(['scripts-theme', 'vendor-theme', 'styles-vendor-theme','styles-sass', 'styles-sass-theme', 'images-theme', 'vectors-theme'],'html-dev', 'browser-sync-theme',  function () {
         gulp.watch(global.paths.scriptSrc, ['scripts-theme']);
         gulp.watch(global.paths.vendorSrc + '/**/*.js', ['vendor-theme']);
         gulp.watch(global.paths.styleSrc, ['styles-sass', 'styles-sass-theme']);
-        //gulp.watch(global.paths.styleSrc + '/**/*.css', ['styles', 'styles-dev']);
-        //gulp.watch(global.paths.spriteSrc, ['sprite']);
         gulp.watch(global.paths.vectorSrc, ['vectors-theme', browserSync.reload]);
         gulp.watch(global.paths.imagesSrc, ['images-theme', browserSync.reload]);
         gulp.watch(global.paths.htmlSrc + '/**/*.html', ['html-dev', browserSync.reload]);
 
     });
 });
-
 
 gulp.task('deploy-static', function () {
     runSequence('zip-static', ['scripts-deploy','styles-deploy', 'html-deploy', 'images-deploy']);
